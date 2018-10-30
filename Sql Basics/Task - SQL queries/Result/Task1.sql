992a94d0b58476e10126981fd18fd5efd71f0ef7
Ilya Dvartsavoi <Ilya_Dvartsavoi@epam.com>
2018-10-30 11:16:29 +0300

    + Added script with first task

---
+++ Sql Basics/Task - SQL queries/Result/Task1.sql
@@ -0,0 +1,7 @@
+﻿select O.CustomerID, SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) as OrderSum from Customers as C
+join Orders as O
+on C.CustomerID = O.CustomerID
+join [Order Details] as OD
+on O.OrderID = OD.OrderID
+group by O.CustomerID
+order by OrderSum desc
