6518e1ada2d21022684c468584430536d5fac325
Ilya Dvortsevoy <ilya.dvortsevoy@gmail.com>
2018-11-06 15:30:03 +0300

    + some changes

--- Entity Framework/EF Task/DAL/Context/AppDbContext.cs
+++ Entity Framework/EF Task/DAL/Context/AppDbContext.cs
@@ -19,6 +19,60 @@ namespace DAL.Context
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             //TODO: setup dbcontext configuration based on "InternationWidgets.mdf" file using EF Fluent API. DO NOT modify any files from "Entities" folder.
+
+            modelBuilder.Entity<Customer>()
+                .ToTable("customers")
+                .HasKey(c => c.CId);
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.CId)
+                .HasColumnName("customer_id");
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.CAddress)
+                .HasColumnType("nvarchar(MAX)")
+                .HasColumnName("customer_address")
+                .IsOptional();
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.CCity)
+                .HasColumnType("nvarchar(MAX)")
+                .HasColumnName("customer_city")
+                .IsOptional();
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.CName)
+                .HasColumnType("nvarchar(MAX)")
+                .HasColumnName("customer_name")
+                .IsOptional();
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.CState)
+                .HasColumnType("nvarchar(MAX)")
+                .HasColumnName("customer_state")
+                .IsOptional();
+
+            modelBuilder.Entity<Order>()
+                .ToTable("orders")
+                .HasKey(o => o.OId)
+                .HasRequired<Customer>(o => o.OCustomer)
+                .WithMany(c => c.COrders)
+                .HasForeignKey(o => o.OCustomer.CId);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.OId)
+                .HasColumnName("order_id");
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ODate)
+                .HasColumnType("datetime")
+                .HasColumnName("order_date")
+                .IsRequired();
+            modelBuilder.Entity<Order>()
+                .Property(o => o.OCustomer.CId)
+                .HasColumnType("int")
+                .HasColumnName("customer_id");
+
+
+                
+                
+
+
+
+
             modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
---
+++ Sql Basics/Task - SQL queries/Result/DQL_Scripts.sql
@@ -0,0 +1,83 @@
+--Task1
+SELECT O.CustomerID, SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS OrderSum
+FROM Customers AS C
+join Orders AS O
+ON C.CustomerID = O.CustomerID
+join [Order Details] AS OD
+ON O.OrderID = OD.OrderID
+GROUP BY O.CustomerID
+ORDER BY OrderSum DESC;
+
+--Task2
+--without grouping
+SELECT C.CustomerID, C.CompanyName AS CustomerName, S.SupplierID, S.CompanyName AS SupplierName
+FROM Customers AS C
+LEFT JOIN Suppliers AS S
+ON C.City = S.City AND C.Country = S.Country
+ORDER BY C.CustomerID;
+
+--with grouping
+SELECT C.CustomerID, C.CompanyName AS CustomerName, COUNT(S.SupplierID) AS Match
+FROM Customers AS C
+LEFT JOIN Suppliers AS S
+ON C.City = S.City AND C.Country = S.Country
+GROUP BY C.CustomerID, C.CompanyName
+ORDER BY C.CustomerID;
+
+--Task3 
+SELECT C.CompanyName, MAX(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS MaxOrderSum
+FROM Customers AS C
+INNER JOIN Orders AS O
+ON C.CustomerID = O.CustomerID
+INNER JOIN [Order Details] AS OD
+ON O.OrderID = OD.OrderID
+GROUP BY C.CompanyName
+HAVING MAX(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) > 500
+ORDER BY MAX(OD.UnitPrice * OD.Quantity * (1 - OD.Discount));
+
+--Task4
+SELECT	C.CustomerID, 
+		C.CompanyName, 
+		YEAR(MIN(O.OrderDate)) AS InitialYear,
+		MONTH(MIN(O.OrderDate)) AS InitialMonth
+FROM Customers AS C
+INNER JOIN Orders AS O
+ON C.CustomerID = O.CustomerID
+GROUP BY C.CustomerID, C.CompanyName;
+
+--Task5
+SELECT	C.CustomerID, 
+		C.CompanyName, 
+		YEAR(MIN(O.OrderDate)) AS InitialYear,
+		MONTH(MIN(O.OrderDate)) AS InitialMonth, 
+		SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS OrderSum
+FROM Customers AS C
+INNER JOIN Orders AS O
+ON C.CustomerID = O.CustomerID
+INNER JOIN [Order Details] AS OD
+ON O.OrderID = OD.OrderID
+GROUP BY C.CustomerID, C.CompanyName
+ORDER BY InitialYear, InitialMonth, OrderSum DESC, C.CompanyName;
+
+--Task6
+SELECT CustomerID, CompanyName, ContactName, PostalCode, Region, Phone FROM Customers
+WHERE	ISNUMERIC(PostalCode) = 0 OR
+		Region IS NULL OR
+		Phone NOT LIKE '(%';
+
+--Task7
+SELECT	C.CategoryName,
+		CASE
+			WHEN P.UnitsInStock > 0 Then 'True'
+			WHEN P.UnitsInStock = 0 Then 'False'
+		END AS OnStock,
+		COUNT(P.ProductID) AS ProductCount, 
+		SUM(P.UnitPrice * P.UnitsInStock) AS TotalSum
+FROM Categories AS C
+INNER JOIN Products AS P
+ON C.CategoryID = P.CategoryID
+GROUP BY C.CategoryName, CASE	WHEN P.UnitsInStock > 0 Then 'True'
+								WHEN P.UnitsInStock = 0 Then 'False' END
+ORDER BY TotalSum;
+
+--Task8
