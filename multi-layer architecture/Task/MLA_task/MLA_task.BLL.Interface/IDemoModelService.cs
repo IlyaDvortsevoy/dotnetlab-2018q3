25a805583e2a12d5f1cefa8f3f538e873cd75e73
Ilya Dvortsevoy <ilya.dvortsevoy@gmail.com>
2018-11-12 21:43:46 +0300

    + Add GetAllDemoModelsAsync,  AddDemoModelAsync methods to IDemoModelService interface

--- multi-layer architecture/Task/MLA_task/MLA_task.BLL.Interface/IDemoModelService.cs
+++ multi-layer architecture/Task/MLA_task/MLA_task.BLL.Interface/IDemoModelService.cs
@@ -1,10 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using MLA_task.BLL.Interface.Models;
 
 namespace MLA_task.BLL.Interface
 {
     public interface IDemoModelService
     {
+        Task<IEnumerable<DemoModel>> GetAllDemoModelsAsync();
+
         Task<DemoModel> GetDemoModelByIdAsync(int id);
+
+        Task AddDemoModelAsync(DemoModel model);
     }
 }
