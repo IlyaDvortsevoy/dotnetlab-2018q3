2afbc58c7744cc011672522ad3ed825eb67d1cd6
Ilya Dvortsevoy <ilya.dvortsevoy@gmail.com>
2018-11-12 21:39:47 +0300

    + Add GetAllDemoModelsAsync,  AddDemoModelAsync methods to MLA_task.BLL.Interface

--- multi-layer architecture/Task/MLA_task/MLA_task.BLL/DemoModelService.cs
+++ multi-layer architecture/Task/MLA_task/MLA_task.BLL/DemoModelService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using MLA_task.BLL.Interface;
 using MLA_task.BLL.Interface.Exceptions;
 using MLA_task.BLL.Interface.Models;
 using MLA_task.DAL.Interface;
+using MLA_task.DAL.Interface.Entities;
 
 namespace MLA_task.BLL
 {
@@ -17,6 +16,22 @@ namespace MLA_task.BLL
         public DemoModelService(IDemoDbModelRepository demoDbModelRepository)
         {
             _demoDbModelRepository = demoDbModelRepository;
+        }
+
+        public async Task<IEnumerable<DemoModel>> GetAllDemoModelsAsync()
+        {
+            var dbModels = await _demoDbModelRepository.GetAllAsync();
+
+            var demoModels = dbModels.Select(dbM => new DemoModel
+            {
+                Id = dbM.Id,
+                Name = dbM.Name,
+                Created = dbM.Created,
+                Modified = dbM.Modified,
+                CommonInfo = dbM.DemoCommonInfoModel.CommonInfo
+            });
+
+            return demoModels;
         }
 
         public async Task<DemoModel> GetDemoModelByIdAsync(int id)
@@ -38,6 +53,18 @@ namespace MLA_task.BLL
             };
 
             return demoModel;
+        }
+
+        public async Task AddDemoModelAsync(DemoModel model)
+        {
+            var dbModel = new DemoDbModel { Name = model.Name };
+
+            var info = await _demoDbModelRepository
+                .GetCommonInfoByCommonInfoAsync(model.CommonInfo);
+
+            dbModel.DemoCommonInfoModelId = info?.Id ?? 1;
+
+            await _demoDbModelRepository.AddAsync(dbModel);
         }
     }
 }
