e0799b298028a50dec5f61aa62324bbb427b4d9e
Ilya Dvortsevoy <ilya.dvortsevoy@gmail.com>
2018-11-19 17:09:47 +0300

    + Sixth task error

--- LINQ/Task/Task/LinqSamples.cs
+++ LINQ/Task/Task/LinqSamples.cs
@@ -9,6 +9,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using SampleSupport;
@@ -25,41 +26,179 @@ namespace SampleQueries
 
 		private DataSource dataSource = new DataSource();
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 1")]
-		[Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
-		public void Linq1()
-		{
-			int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 1")]
+	    [Description("This sample gets a list of all customers whose total turnover (the sum of all orders) exceeds some value of X.")]
+	    public void Linq1()
+	    {
+	        decimal turnover = 200M;
 
-			var lowNums =
-				from num in numbers
-				where num < 5
-				select num;
+            var customers =
+	            from customer in dataSource.Customers
+                let sum = customer.Orders.Sum(order => order.Total)
+                where sum > turnover
+                select new
+                {
+                    ID = customer.CustomerID,
+                    CompanyName = customer.CompanyName,
+                    OrdersSum = sum
+                };
 
-			Console.WriteLine("Numbers < 5:");
-			foreach (var x in lowNums)
-			{
-				Console.WriteLine(x);
-			}
-		}
+	        Console.WriteLine("Query with \"turnover\" variable equaled to 200");
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
+	        foreach (var customer in customers)
+	        {
+	            ObjectDumper.Write(customer);
+	        }
 
-		public void Linq2()
-		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
+	        turnover = 700M; // To avoid of copying the query several times
 
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
+	        Console.WriteLine("Query with \"turnover\" variable equaled to 700");
 
-	}
+            foreach (var customer in customers)
+	        {
+	            ObjectDumper.Write(customer);
+	        }
+        }
+
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 2 with grouping")]
+	    [Description("This sample makes a list of suppliers located in the same country and the same city for each client using grouping.")]
+	    public void Linq2()
+	    {
+	        var customerSuppliers =
+	            from customer in dataSource.Customers
+	            join supplier in dataSource.Suppliers on
+	                new {customer.Country, customer.City} equals new {supplier.Country, supplier.City}
+	                into match
+	            from customerSupplier in match
+	            select new
+	            {
+	                customer.CompanyName,
+	                customer.Country,
+	                customer.City,
+	                customerSupplier.SupplierName
+	            };
+
+            foreach (var customerSupplier in customerSuppliers)
+	        {
+	            ObjectDumper.Write(customerSupplier);
+	        }
+	    }
+
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 2")]
+	    [Description("This sample makes a list of suppliers located in the same country and the same city for each client without grouping.")]
+	    public void Linq3()
+	    {
+	        var customerSuppliers =
+	            from customer in dataSource.Customers
+	            from supplier in dataSource.Suppliers
+                where supplier.Country == customer.Country && supplier.City == customer.City
+	            select new
+	            {
+	                customer.CompanyName,
+	                customer.Country,
+	                customer.City,
+	                supplier.SupplierName
+	            };
+
+            foreach (var customerSupplier in customerSuppliers)
+	        {
+	            ObjectDumper.Write(customerSupplier);
+	        }
+	    }
+
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 3")]
+	    [Description("This sample finds all customers who have orders that exceed the sum of X.")]
+	    public void Linq4()
+	    {
+	        decimal orderTotal = 1500M;
+
+	        var customers =
+	            from customer in dataSource.Customers
+	            from order in customer.Orders
+	            where order.Total > orderTotal
+                orderby customer.CompanyName
+                select new
+	            {
+	                Company = customer.CompanyName,
+	                order.OrderID,
+                    order.Total
+	            };
+
+	        foreach (var customer in customers)
+	        {
+	            ObjectDumper.Write(customer);
+	        }
+	    }
+
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 4")]
+	    [Description("This sample gets a list of customers including the month of the year they became clients.")]
+	    public void Linq5()
+	    {
+	        var customers =
+	            from customer in dataSource.Customers
+	            where customer.Orders.Count() > 0
+                let minDate = customer.Orders.Min(order => order.OrderDate)
+                select new
+	            {
+	                Company = customer.CompanyName,
+                    ClientFrom = minDate
+                };
+
+            foreach (var customer in customers)
+            {
+                ObjectDumper.Write(customer);
+            }
+        }
+
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 5")]
+	    [Description("This sample gets a list of customers including the month of the year they became clients sorted by year, month, customer turnover (from the maximum to the minimum), and the client's name.")]
+	    public void Linq6()
+	    {
+	        var customers =
+	            from customer in dataSource.Customers
+	            where customer.Orders.Count() > 0
+	            let minDate = customer.Orders.Min(order => order.OrderDate)
+                let turnover = customer.Orders.Sum(order => order.Total)
+                orderby minDate.Year, minDate.Month, turnover descending, customer.CompanyName
+                select new
+	            {
+	                Company = customer.CompanyName,
+	                ClientFrom = minDate,
+	                Turnover = turnover
+                };
+
+	        foreach (var customer in customers)
+	        {
+	            ObjectDumper.Write(customer);
+	        }
+	    }
+
+	    [Category("LINQ Module Tasks")]
+	    [Title("Task 6")]
+	    [Description("This sample specifies all customers who have a non-digital postal code, or the region is not filled, or the operator code is not specified in the phone.")]
+	    public void Linq7()
+	    {
+	        string template = @"\d*";
+	        var regex = new Regex(template);
+
+	        var customers =
+	            from customer in dataSource.Customers
+                let validPostalCode = regex.IsMatch(customer.PostalCode)
+                where validPostalCode == false ||
+                      customer.Region == null ||
+                      customer?.Phone[0] != '('
+                select new { Company = customer.CompanyName };
+
+	        foreach (var customer in customers)
+	        {
+	            ObjectDumper.Write(customer);
+	        }
+	    }
+    }
 }
