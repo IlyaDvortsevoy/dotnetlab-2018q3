d9a60f0012bb7f593845e1e588f10e63fa91afd0
Ilya Dvortsevoy <ilya.dvortsevoy@gmail.com>
2018-11-21 17:09:34 +0300

    + Add restService solution

---
+++ RESTful api/RestService/RestService/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestService.Controllers
+{
+    [RoutePrefix("users")]
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        [Route("get")]        
+        public IHttpActionResult Get()
+        {
+            return Ok("OK!");
+        }
+    }
+}
